fa1cc9f1694738883f485b787451a59b4601bfb3
stanislove <stashevvsky@gmail.com>
2023-04-14 00:41:10 +0100

    add execution number and parallelism env variables

--- gas_consumption.py
+++ gas_consumption.py
@@ -2,9 +2,11 @@ import os
 import subprocess
 import json
 import uuid
-import array
+import csv
+import concurrent.futures
 
-
+EXECUTIONS_NUMBER = int(os.environ['EXECUTIONS_NUMBER'])
+EXECUTION_PARALLELISM = int(os.environ['EXECUTION_PARALLELISM'])
 BINARY_PATH = os.environ['BINARY_PATH']
 WALLET_NAME = os.environ['WALLET_NAME']
 WALLET_PASSPHRASE = os.environ['WALLET_PASSPHRASE']
@@ -76,27 +78,31 @@ class TransactionInfo:
         self.gas_used = gas_used
         self.raw_response = raw_response
 
-    def __str__(self):
-        return f"TransactionInfo: " \
-            f"operation_name={self.operation_name}, gas_wanted={self.gas_wanted}," \
-            f"gas_used={self.gas_used}, raw_response={self.raw_response}"
+
+    def to_dict(self):
+        return {'operation_name': self.operation_name, 'gas_wanted': self.gas_wanted, 'gas_used': self.gas_used}
 
 
 def execute(command, operation_name):
-    result = subprocess.check_output(command, shell=True).decode('utf-8')
+    error_tx = TransactionInfo('N/A', 'N/A', 'N/A', 'N/A')
+    try:
+        result = subprocess.check_output(command, shell=True).decode('utf-8')
+    except Exception:
+        return error_tx
+
     result_json = json.loads(result)
-    if isinstance(result_json.get('raw_log', ''), array.array):
-        return TransactionInfo(operation_name, result_json.get('gas_wanted', 'N/A'), result_json['gas_used'], result)
+    if result_json.get('code', 0) == 0:
+        gas_used = result_json['gas_used'] if 'gas_used' in result_json else result_json['data']['gas_used']
+        return TransactionInfo(operation_name, result_json.get('gas_wanted', 'N/A'), gas_used, result)
     else:
-        return TransactionInfo('N/A', 'N/A', 'N/A', result)
+        # if there was an error - ignore such transaction
+        return error_tx
 
 
 
 def get_objectarium_object_id(store_response):
     parsed_data = json.loads(store_response)
-    print(parsed_data)
     for log in parsed_data['logs']:
-        print(log)
         for event in log['events']:
             for attr in event['attributes']:
                 if attr['key'] == 'id':
@@ -104,19 +110,42 @@ def get_objectarium_object_id(store_response):
                     return id_value
 
 
+def execute_transactions():
+    tx_info_array = []
+    for i in range(EXECUTIONS_NUMBER):
+        store_response = execute(OBJECTARIUM_STORE_TEMPLATE.format(data=uuid.uuid4()), 'store_object')
+        if store_response.operation_name != 'N/A':
+            object_id = get_objectarium_object_id(store_response.raw_response)
+            tx_info_array.append(store_response)
+
+            pin_object_response = execute(OBJECTARIUM_PIN_OBJECT_TEMPLATE.format(object_id=object_id), 'pin_object')
+            tx_info_array.append(pin_object_response)
+            unpin_object_response = execute(OBJECTARIUM_UNPIN_OBJECT_TEMPLATE.format(object_id=object_id), 'unpin_object')
+            tx_info_array.append(unpin_object_response)
+            forget_object_response = execute(OBJECTARIUM_FORGET_OBJECT_TEMPLATE.format(object_id=object_id), 'forget_object')
+            tx_info_array.append(forget_object_response)
+
+        can_change_governance_response = execute(LAW_STONE_CHANGE_GOVERNANCE, 'change_governance')
+        tx_info_array.append(can_change_governance_response)
+    return tx_info_array
+
+
 if __name__ == '__main__':
     tx_info_array = []
-    store_response = execute(OBJECTARIUM_STORE_TEMPLATE.format(data=uuid.uuid4()), 'store_object')
-    object_id = get_objectarium_object_id(store_response.raw_response)
-    tx_info_array.append(store_response)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for i in range(EXECUTION_PARALLELISM):
+            futures.append(executor.submit(execute_transactions))
+        for future in concurrent.futures.as_completed(futures):
+            tx_info_array.extend(future.result())
 
-    pin_object_response = execute(OBJECTARIUM_PIN_OBJECT_TEMPLATE.format(object_id=object_id), 'pin_object')
-    tx_info_array.append(pin_object_response)
-    unpin_object_response = execute(OBJECTARIUM_UNPIN_OBJECT_TEMPLATE.format(object_id=object_id), 'unpin_object')
-    tx_info_array.append(unpin_object_response)
-    forget_object_response = execute(OBJECTARIUM_FORGET_OBJECT_TEMPLATE.format(object_id=object_id), 'forget_object')
-    tx_info_array.append(forget_object_response)
-    can_change_governance_response = execute(LAW_STONE_CHANGE_GOVERNANCE, 'change_governance')
-    tx_info_array.append(can_change_governance_response)
 
-    print(tx_info_array)
+    with open('report.csv', mode='w') as csv_file:
+        writer = csv.writer(csv_file)
+        for tx_info in tx_info_array:
+            print(tx_info.to_dict())
+            if tx_info.operation_name == 'N/A':
+                continue
+            tx_dict = tx_info.to_dict()
+            writer.writerow(tx_dict.keys())
+            writer.writerow(tx_dict.values())
